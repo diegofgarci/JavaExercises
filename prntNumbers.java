a8ba78f2a9ddbed3420e3a0e44ee4706755a9b12
diegofgarci <dfernandezgarcia1@uoc.edu>
2016-12-03 12:15:13 +0100

    Create prntNumbers.java

---
+++ prntNumbers.java
@@ -0,0 +1,8 @@
+public class practica {
+    
+    public static void main(String[] args) {
+        for (int number=1; number<11;number++){
+            System.out.print(number);
+        }
+    }    
+}
