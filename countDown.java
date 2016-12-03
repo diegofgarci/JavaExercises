23e858648cc148ad181dd3c55762450c9fd4ccdd
diegofgarci <dfernandezgarcia1@uoc.edu>
2016-12-03 12:18:24 +0100

    Create countDown.java

---
+++ countDown.java
@@ -0,0 +1,8 @@
+public class practica {
+    
+    public static void main(String[] args) {
+        for (int number=10; number>=0;number--){
+            System.out.print(number+",");
+        }
+    }    
+}
