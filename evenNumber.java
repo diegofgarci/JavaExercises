c3fcf28243c58609a0d4f7f5520ee1e2b75a0356
diegofgarci <dfernandezgarcia1@uoc.edu>
2016-12-03 15:54:33 +0100

    Create evenNumber.java

---
+++ evenNumber.java
@@ -0,0 +1,14 @@
+public class practica {
+    
+    public static void main(String[] args) {
+       int number=0;
+       while(number<=100){
+           if (number%2==0){
+               System.out.print(number+", ");
+           }
+           number++;
+       }
+       
+      
+    }    
+}
