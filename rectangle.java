84e8456a4b17ee1976c3785dd86bab5c5787858f
diegofgarci <dfernandezgarcia1@uoc.edu>
2016-12-03 11:53:05 +0100

    Create rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,21 @@
+public class rectangle {
+    
+    public static void main(String[] args) {
+        int height=3;
+        int width=10;
+        int rowCounter=0;
+        int columnCounter=0;
+        
+        while (rowCounter<height){
+            columnCounter=0;
+            while (columnCounter<10){
+                System.out.print("@");
+                columnCounter++;
+            }
+          System.out.println();
+          rowCounter++;
+        }
+      
+    }
+    
+}
