9c4c9dc2a83bf145121fca9206835f11c0ef8061
diegofgarci <dfernandezgarcia1@uoc.edu>
2016-12-04 19:22:10 +0100

    Create factorial.java
    This program calculates the factorial of a number. 3!=1*2*3.

---
+++ factorial.java
@@ -0,0 +1,10 @@
+public class practica {
+    public static void main(String[] args) {
+        int fact=8;
+        int result=1;
+        for (int i=1; i<=fact; i++){
+            result=result*i;
+            }
+        System.out.print(result);
+        }
+}        
