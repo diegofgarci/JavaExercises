93c03b86005e19a46c7cd82110a77b85246684f1
diegofgarci <dfernandezgarcia1@uoc.edu>
2017-05-15 21:38:16 +0200

    Update ejercicioMatriz.java

--- ejercicioMatriz.java
+++ ejercicioMatriz.java
@@ -5,27 +5,7 @@ public class EjercicioMatriz {
     public static void main(String args[]) {
 
         //Dada la siguiente matriz
-        int[][] m = new int[4][4];
-
-        m[0][0] = 1;
-        m[0][1] = 4;
-        m[0][2] = 5;
-        m[0][3] = 7;
-
-        m[1][0] = 2;
-        m[1][1] = 3;
-        m[1][2] = 5;
-        m[1][3] = 8;
-
-        m[2][0] = 0;
-        m[2][1] = 9;
-        m[2][2] = 2;
-        m[2][3] = 7;
-
-        m[3][0] = 3;
-        m[3][1] = 5;
-        m[3][2] = 1;
-        m[3][3] = 2;
+        int[][] m = {{1,4,5,7}, {2,3,5,8}, {0,9,2,7}, {3,5,1,2}};
 
         //Imprimir el contenido de la matriz
         for (int i = 0; i < m.length; i++) {
